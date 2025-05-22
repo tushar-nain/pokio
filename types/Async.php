d27806e2feded98469616c6ac9df1e07b7a6b890
Nuno Maduro <enunomaduro@gmail.com>
2025-05-23 00:35:19 +0100

    feat: type safety api

--- types/Async.php
+++ types/Async.php
@@ -10,3 +10,23 @@ assertType('int', await($promise));
 $promiseA = async(fn () => 'string');
 $promiseB = async(fn () => 'string');
 assertType('array<int, string>', await([$promiseA, $promiseB]));
+
+$promise = async(fn () => 1)
+    ->then(fn (int $result) => $result * 2);
+
+assertType('int', await($promise));
+
+$promise = async(fn () => 1)
+    ->then(fn (int $result) => 'string');
+
+assertType('string', await($promise));
+
+$promise = async(fn () => throw new Exception())
+    ->catch(fn (Throwable $th) => 1);
+
+assertType('int', await($promise));
+
+$promise = async(fn () => throw new Exception())
+    ->catch(fn (Throwable $th) => throw new Exception());
+
+assertType('never', await($promise));
