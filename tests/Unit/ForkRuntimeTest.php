be5a6f0ec86c5c3cd9617ef9f63356674a8bb2ce
Constantin Ross <constantin.ross@stud.hn.de>
2025-05-23 18:45:11 +0200

    feat (tests): add fork runtime test

---
+++ tests/Unit/ForkRuntimeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Pokio\Environment;
+
+test('fork runtime waits if there are too many processes', function (): void {
+    $maxProcesses = Environment::maxProcesses();
+    $numberOfProcesses = $maxProcesses + 1;
+
+    $promises = [];
+
+    for ($i = 0; $i < $numberOfProcesses; $i++) {
+        $promises[] = async(function () {
+            return 1;
+        });
+    }
+
+    $results = await($promises);
+    expect($results)->toHaveCount($numberOfProcesses);
+})->with('runtimes');
